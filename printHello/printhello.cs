376c01b7d4cc7fbea76021b3188a5554ac5173d5
Julhas <mdjulhashossainmohon@gmail.com>
2017-07-08 15:36:52 +0600

    Here is first code of c# using ubuntu mono pakage

---
+++ printHello/printhello.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace printHello
+{
+  class printHello
+  {
+    public static void Main(string[] args)
+    {
+      Console.WriteLine("Hello this is a csharp Application");
+    }
+  }
+}
