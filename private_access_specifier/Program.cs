5a63ed129253e74e2efe9beefcb4eb18bee17d26
Maxyee <eyaminkhan00@gmail.com>
2017-10-29 15:47:19 +0600

    private access specifier in csharp

---
+++ private_access_specifier/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace private_access_specifier
+{
+
+    // main method class for private access specifier encapsulation cSharp
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Rectangle julhas = new Rectangle();
+            julhas.Acceptdetails();
+            julhas.Display();
+            Console.ReadKey();
+        }
+    }
+}
---
+++ private_access_specifier/Rectangle.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace private_access_specifier
+{
+    // private access specifier encapsulation in cSharp....
+    class Rectangle
+    {
+        private double length;
+        private double width;
+
+        public double GetArea()
+        {
+            return length*width;
+        }
+
+        public void Acceptdetails()
+        {
+            Console.WriteLine("Enter Length: ");
+            length = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Enter Width: ");
+            width = Convert.ToDouble(Console.ReadLine());
+        }
+
+        public void Display()
+        {
+            Console.WriteLine("Length: {0}",length);
+            Console.WriteLine("Width: {0}",width);
+            Console.WriteLine("Area: {0}",GetArea());
+        }
+    }
+}
