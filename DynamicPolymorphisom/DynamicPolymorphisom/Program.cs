cd8c2b82d49a21a0f516f41c8acbec9f110447c6
maxyee <mdjulhashossainmohon@gmail.com>
2017-11-07 00:54:33 +0600

    Learned how to use Dynamic polymorphisom with abstract class

---
+++ DynamicPolymorphisom/DynamicPolymorphisom/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DynamicPolymorphisom
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Rectangle rect = new Rectangle(10,7);
+            double a = rect.area();
+            Console.WriteLine("Area {0}",a);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ DynamicPolymorphisom/DynamicPolymorphisom/Rectangle.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DynamicPolymorphisom
+{
+    class Rectangle : Shape
+    {
+        private int lenght;
+        private int width;
+
+        public Rectangle(int l = 0, int w = 0)
+        {
+            lenght = l;
+            width = w;
+        }
+
+        public override int area()
+        {
+            Console.WriteLine("Rectangle class area :");
+            return (width*lenght);
+        }
+    }
+}
