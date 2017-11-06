f9b090a4434bd60a0c138947bd5f4b68a566ab4d
maxyee <mdjulhashossainmohon@gmail.com>
2017-11-06 23:18:01 +0600

    Learned how the nullable things work in cSharp

---
+++ csharp_nullable/Delegate.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+internal delegate int NumberChanger(int n);
+
+namespace csharp_nullable
+{
+    class Delegate
+    {
+        public static int num = 10;
+
+        public static int AddNum(int p)
+        {
+            num = num + p;
+            return num;
+        }
+
+        public static int MultiNum(int q)
+        {
+            num = num*q;
+            return num;
+        }
+
+        public static int getNum()
+        {
+            return num;
+        }
+
+        public static void Main(string[] args)
+        {
+            NumberChanger nc1 = new NumberChanger(AddNum);
+            NumberChanger nc2 = new NumberChanger(MultiNum);
+
+            nc1(25);
+            Console.WriteLine("Value of Num: {0}",getNum());
+
+            nc2(5);
+            Console.WriteLine("Value of Num: {0}",getNum());
+            Console.ReadKey();
+        }
+
+    }
+}
---
+++ csharp_nullable/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace csharp_nullable
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int? num1 = null;
+            int? num2 = 45;
+
+            double? num3 = new double?();
+            double? num4 = 3.1416;
+
+            bool? value = new bool?();
+
+            Console.WriteLine("Nullable value: {0},{1},{2},{3}", num1,num2,num3,num4 );
+            Console.WriteLine("Boolean value: {0}", value);
+            Console.ReadKey();
+        }
+    }
+}
