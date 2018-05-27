0ba46e3ef051d167e4ea5dc0f72fb2e2e5ff1cd2
Maxyee <eyaminkhan00@gmail.com>
2018-05-28 04:33:22 +0600

    csharp attribute section

---
+++ CSharpAttributes/Test.cs
@@ -0,0 +1,40 @@
+﻿#define DEBUG
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+
+namespace CSharpAttributes
+{
+    public class MyClass
+    {
+        [Conditional("DEBUG")]
+
+        public static void Message(string msg)
+        {
+            Console.WriteLine(msg);
+        }
+    }
+    class Test
+    {
+        static void function1()
+        {
+            MyClass.Message("In Function 1.");
+            function2();
+        }
+
+        static void function2()
+        {
+            MyClass.Message("In Function 2.");
+        }
+
+        public static void Main(string[] args)
+        {
+            MyClass.Message("In Main function.");
+            function1();
+            Console.ReadKey();
+        }
+    }
+}
