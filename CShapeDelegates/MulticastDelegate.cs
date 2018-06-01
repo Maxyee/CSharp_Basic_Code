f4da5baade55f7d8b30a89a11aa670f8aa6bc81b
Maxyee <eyaminkhan00@gmail.com>
2018-06-02 03:01:13 +0600

    Delegate method and how to use it into the code

---
+++ CShapeDelegates/MulticastDelegate.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+delegate int NumberChangerMulticast(int n);
+namespace CShapeDelegates
+{
+    class MulticastDelegate
+    {
+        static int num = 10;
+
+        public static int AddNum(int k)
+        {
+            num += k;
+            return num;
+        }
+
+        public static int MutiNum(int j)
+        {
+            num *= j;
+            return num;
+        }
+
+        public static int getNum()
+        {
+            return num;
+        }
+
+        static void Main(string[] args)
+        {
+            NumberChangerMulticast nc;
+            NumberChangerMulticast nc1 = new NumberChangerMulticast(AddNum);
+            NumberChangerMulticast nc2 = new NumberChangerMulticast(MutiNum);
+
+            nc = nc1;
+            nc += nc2;
+
+            nc(5);
+            Console.WriteLine("Value of Num: {0}", getNum());
+            Console.ReadKey();
+            
+
+        }
+    }
+}
---
+++ CShapeDelegates/TestDelegate.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+delegate int NumberChanger(int n);
+namespace CShapeDelegates
+{
+    class TestDelegate
+    {
+        static int num = 10;
+
+        public static int AddNum(int p)
+        {
+            num += p;
+            return num;
+        }
+
+        public static int MultiplyNum(int z)
+        {
+            num *= z;
+            return num;
+        }
+
+        public static int getNum()
+        {
+            return num;
+        }
+        static void Main(string[] args)
+        {
+            NumberChanger nc1 = new NumberChanger(AddNum);
+            NumberChanger nc2 = new NumberChanger(MultiplyNum);
+
+            nc1(25);
+            Console.WriteLine("Value of Num: {0}", getNum());
+
+            nc2(5);
+            Console.WriteLine("Value of Num: {0}", getNum());
+            Console.ReadKey();
+        }
+    }
+}
