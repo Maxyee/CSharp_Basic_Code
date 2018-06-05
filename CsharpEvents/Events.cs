0fdeae07e9be6e19bd2137cc01ace154dfbb78ec
Maxyee <eyaminkhan00@gmail.com>
2018-06-05 23:38:11 +0600

    Learned how to use Csharp events and why events are nessasery

---
+++ CsharpEvents/Events.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CsharpEvents
+{
+    public delegate string MyDel(string str);
+    class Events
+    {
+        event MyDel MyEvent;
+
+        public Events()
+        {
+            this.MyEvent += new MyDel(this.WelcomeUser);
+        }
+
+        public string WelcomeUser(string username)
+        {
+            return "Welcome" + username;
+        }
+        static void Main(string[] args)
+        {
+            Events obj1 = new Events();
+            string result = obj1.MyEvent("Julhas Hossain");
+            Console.WriteLine(result);
+        }
+    }
+}
