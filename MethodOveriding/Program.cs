806288d16be6a1063cf9dcf9521592a4bde18993
Maxyee <eyaminkhan00@gmail.com>
2018-09-20 17:05:14 +0600

    Method Overriding code writted in c#

---
+++ MethodOveriding/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodOveriding
+{
+    class Baseclass
+    {
+        public virtual void Greetings()
+        {
+            Console.WriteLine("baseClass Saying Hello!");
+        }
+    }
+
+    class Subclass:Baseclass
+    {
+        public override void Greetings()
+        {
+            base.Greetings();
+            Console.WriteLine("subClass Saying Hello");
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Baseclass obj1 = new Subclass();
+            obj1.Greetings();
+            Console.ReadLine();
+        }
+    }
+}
